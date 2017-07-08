4f0a123469e4ed0dd01e059e9f1267f7f1b17cf4
Olorunda Victor <victorolorunda@yahoo.com>
2017-07-08 17:28:27 +0100

    I added nore funtionalities to the app

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,45 @@
 (function(){
-   var minutes = 0, seconds = 0, hours = 0;
+  var clock, count = 0;
+  let strt = document.getElementById('strt'),
+      name = document.getElementById('nm');
 
-      // create a text saying Digital clock plugin.
-      var p = document.createElement("p");
-      // create the clock.
-      var clock = setInterval(function(){
-        p.textContent = hours + " : " + minutes + " : " + seconds++;
-        p.className = "large";
-        document.body.appendChild(p);
+    strt.addEventListener("click", function() {
+      if(count < 1){
+          if(name.value.length > 1){
+            count++;
+            stop();
+            start(name.value);
+          }
+      }
+      else {
 
-        // increase the minutes.
-        if(seconds == 60){
-          minutes++;
-          seconds = 0;
-        }
+      }
+    })
 
-        if(minutes == 60){
-          hours++;
-          minutes = 0;
-        }
+  function start(name) {
+        var minutes = 0, seconds = 0, hours = 0;
 
-      }, 1000);
+        // create a text saying Digital clock plugin.
+        var p = document.createElement("p");
+        // create the clock.
+        clock = setInterval(function(){
+          p.textContent = name + ", the time is " + hours + " : " + minutes + " : " + seconds++;
+          p.className = "large";
+          document.body.appendChild(p);
 
+          // increase the minutes.
+          if(seconds == 60){
+            minutes++;
+            seconds = 0;
+          }
+
+          if(minutes == 60){
+            hours++;
+            minutes = 0;
+          }
+
+        }, 1000);
+  }
       function stop(){
           var btn = document.getElementById("btn");
           btn.addEventListener("click", function() {
