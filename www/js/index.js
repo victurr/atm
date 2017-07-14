f41b300f1a29ed9500181d2d9e098d14d4b45ad7
Olorunda Victor <victorolorunda@yahoo.com>
2017-07-14 23:58:45 +0100

    14/07/17 - 23:58

--- www/js/index.js
+++ www/js/index.js
@@ -1,65 +1,70 @@
 document.addEventListener("deviceready", onDeviceReady, false);}
-    function onDeviceReady() {
-      var date = new Date(), minutes = date.getMinutes(), seconds = date.getSeconds(), hours = date.getHours(),
-      clock, count = 0;
 
-      let strt = document.getElementById('strt'),
-      name = document.getElementById('nm'),
-      form = document.getElementById("fm"),
-      tm = document.getElementById("tm");
-
-      strt.addEventListener("click", function() {
-        if(count < 1){
-          if(name.value.length > 1){
-            count++;
-            stop();
-            start(name.value);
-          }
-        }
-        else {
-          // restart the time  by clearing the tag
-
-        }
-      });
-
-      function ap() {
-        if (hours > 11) {
-          return " pm";
-        }
-        else {
-          return  " am";
-        }
+      function onDeviceReady() {
+        alert("works..");
       }
 
-      function start(name) {
-        // create the clock.
-        var clock = setInterval(function(){
-          tm.innerHTML = name + ", the time is " + hours + " : " + minutes + " : " + seconds++ + ap();
-
-          // increase the minutes.
-          if(seconds == 60){
-            minutes++;
-            seconds = 0;
-          }
-
-          if(minutes == 60){
-            hours++;
-            minutes = 0;
-          }
-
-          if(hours == 24){
-            hours = 0;
-            minutes = 0;
-            seconds = 0;
-          }
-        }, 1000);
-      }
-
-      function stop(){
-        var btn = document.getElementById("btn");
-        btn.addEventListener("click", function() {
-          clearInterval(clock);
-        })
-      }
-      stop();
-  }
+  //   function onDeviceReady() {
+  //     var date = new Date(), minutes = date.getMinutes(), seconds = date.getSeconds(), hours = date.getHours(),
+  //     clock, count = 0;
+  //
+  //     let strt = document.getElementById('strt'),
+  //     name = document.getElementById('nm'),
+  //     form = document.getElementById("fm"),
+  //     tm = document.getElementById("tm");
+  //
+  //     strt.addEventListener("click", function() {
+  //       if(count < 1){
+  //         if(name.value.length > 1){
+  //           count++;
+  //           stop();
+  //           start(name.value);
+  //         }
+  //       }
+  //       else {
+  //         // restart the time  by clearing the tag
+  //
+  //       }
+  //     });
+  //
+  //     function ap() {
+  //       if (hours > 11) {
+  //         return " pm";
+  //       }
+  //       else {
+  //         return  " am";
+  //       }
+  //     }
+  //
+  //     function start(name) {
+  //       // create the clock.
+  //       var clock = setInterval(function(){
+  //         tm.innerHTML = name + ", the time is " + hours + " : " + minutes + " : " + seconds++ + ap();
+  //
+  //         // increase the minutes.
+  //         if(seconds == 60){
+  //           minutes++;
+  //           seconds = 0;
+  //         }
+  //
+  //         if(minutes == 60){
+  //           hours++;
+  //           minutes = 0;
+  //         }
+  //
+  //         if(hours == 24){
+  //           hours = 0;
+  //           minutes = 0;
+  //           seconds = 0;
+  //         }
+  //       }, 1000);
+  //     }
+  //
+  //     function stop(){
+  //       var btn = document.getElementById("btn");
+  //       btn.addEventListener("click", function() {
+  //         clearInterval(clock);
+  //       })
+  //     }
+  //     stop();
+  // }
